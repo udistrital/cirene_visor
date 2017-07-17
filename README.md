e7cf24200a6bb1871695ca89460ba18a11cd3826
Jorge Useche <naturalmentejorge@gmail.com>
2017-07-17 12:48:36 -0500

    Se modifican pasos para que toda la construncción se haga con npm scripts.

--- README.md
+++ README.md
@@ -5,36 +5,45 @@ Openlayers con Material Design Lite
 
 ## Requerimientos:
 
+- NPM: Gestor de Paquetes de NodeJS, gestiona también bibliotecas JS del lado del Cliente
+
+## Otro software usado en la construcción:
+
 - Openlayers v4.0: Biblioteca de Javascript para Mapas en la Web
 - Gulp: Systema de Construcción y Automatización de Flujo de Trabajo
 - Bower: Gestor de Paquetes Web
-- NPM: Gestor de Paquetes de NodeJS, gestiona también bibliotecas JS del lado del Cliente
 
 ## Utiliza recomendaciones de estilo estándar:
 
-- JSHint: http://jshint.com/docs
+- JSHint: http://jshint.com/docs (cambió)
 - JSDoc: http://usejsdoc.org/about-getting-started.html
 
 ## Instalación entorno de desarrollo:
 
-Solo se necesita ejecutar el script llamado *install*
+Solo se necesita ejecutar:
 
 ```bash
-$ ./install
+npm install
+npm run bower:install
 ```
 ## Ejecutar entorno de desarrollo:
 
-Solo se necesita ejecutar el script llamado *run*
+Solo se necesita ejecutar:
 
 ```bash
-$ ./run
+npm start
 ```
 
-Para más comandos?
+## Construir artefactos de la aplicación:
+
+Solo se necesita ejecutar:
 
 ```bash
-$ ./run --help
+npm run build
 ```
+
+Esto crea un directorio ***dist*** con el código "minificado" y el paquete,
+***build/pack.tar.gz*** con todo el código en un único fichero.
 
 ## Despliegue de la aplicación
 
@@ -42,8 +51,8 @@ La aplicación necesita un servidor web (Apache, NGINX u otro).
 
 Los archivos del directorio ***app*** deben ser copiados en el servidor web.
 
-Para entornos de producción puede buscar la aplicación "comprimida" o "minimizada" en ***dist***
-y desplegarla de igual manera que con el código fuente.
+Para entornos de producción puede buscar la aplicación "comprimida" o
+"minimizada" en ***dist*** y desplegarla de igual manera que con el código fuente.
 
 # Enlaces de interés
 - http://openlayers.org/en/latest/examples/
--- gulpfile.js
+++ gulpfile.js
@@ -6,27 +6,29 @@
 var gulp = require('gulp')
 var webserver = require('gulp-webserver')
 var minify = require('gulp-minifier')
-//var babel = require('gulp-babel')
-var merge = require('merge-stream')
+//var babel = require('gulp-babel');
+//var merge = require('merge-stream');
+const tar = require('gulp-tar')
+const gzip = require('gulp-gzip')
+var rm = require('gulp-rm')
 
 gulp.task('webserver', function() {
-    gulp.src('app')
-        .pipe(webserver({
-            host: 'localhost',
-            port: 3000,
-            livereload: true,
-            directoryListing: false,
-            open: true,
-            proxies: [{
-              source: '/geoserver',
-              target: 'http://sig.udistrital.edu.co:8080/geoserver',
-              options: {
-                headers: {
-                  'DEVELOPER': 'juusechec'
-                }
-              }
-            }]
-        }))
+  gulp.src('app').pipe(webserver({
+    host: 'localhost',
+    port: 3000,
+    livereload: true,
+    directoryListing: false,
+    open: true,
+    proxies: [{
+      source: '/geoserver',
+      target: 'http://sig.udistrital.edu.co:8080/geoserver',
+      options: {
+        headers: {
+          'DEVELOPER': 'juusechec'
+        }
+      }
+    }]
+  }))
 })
 
 /*
@@ -36,49 +38,72 @@ gulp.task('watch', ['sass'], function (){
 });
 */
 gulp.task('minify', function() {
-    var allfiles = gulp.src(['app/**/*'])
-        .pipe(minify({
-            minify: true,
-            collapseWhitespace: true,
-            conservativeCollapse: true,
-            minifyJS: true,
-            minifyCSS: true,
-            getKeptComment: function(content, filePath) {
-                var m = content.match(/\/\*![\s\S]*?\*\//img);
-                return m && m.join('\n') + '\n' || '';
-            }
-        }))
-        .pipe(gulp.dest('dist/app'))
-    // http://stackoverflow.com/questions/34398338/uglification-failed-unexpected-character
-    // var jsfiles = gulp.src(['app/**/*.js', '!app/vendor/**/*', '!app/js/mustache-dojo.js'])
-    //     .pipe(babel({
-    //         presets: ['es2015']
-    //     }))
-    //     .pipe(minify({
-    //         minify: true,
-    //         collapseWhitespace: true,
-    //         conservativeCollapse: true,
-    //         minifyJS: true,
-    //         minifyCSS: true,
-    //         getKeptComment: function(content, filePath) {
-    //             var m = content.match(/\/\*![\s\S]*?\*\//img);
-    //             return m && m.join('\n') + '\n' || '';
-    //         }
-    //     }))
-    //     .pipe(gulp.dest('dist/app'))
+  var allfiles = gulp.src(['app/**/*']).pipe(minify({
+    minify: true,
+    collapseWhitespace: true,
+    conservativeCollapse: true,
+    minifyJS: true,
+    minifyCSS: true,
+    getKeptComment: function(content, filePath) {
+      var m = content.match(/\/\*![\s\S]*?\*\//img)
+      return m && m.join('\n') + '\n' || ''
+    }
+  })).pipe(gulp.dest('dist/app'))
+  // http://stackoverflow.com/questions/34398338/uglification-failed-unexpected-character
+  // var jsfiles = gulp.src(['app/**/*.js', '!app/vendor/**/*', '!app/js/mustache-dojo.js'])
+  //     .pipe(babel({
+  //         presets: ['es2015']
+  //     }))
+  //     .pipe(minify({
+  //         minify: true,
+  //         collapseWhitespace: true,
+  //         conservativeCollapse: true,
+  //         minifyJS: true,
+  //         minifyCSS: true,
+  //         getKeptComment: function(content, filePath) {
+  //             var m = content.match(/\/\*![\s\S]*?\*\//img);
+  //             return m && m.join('\n') + '\n' || '';
+  //         }
+  //     }))
+  //     .pipe(gulp.dest('dist/app'))
 
-    // var vendorfiles = gulp.src(['app/vendor/**/*'])
-    //     .pipe(gulp.dest('dist/app/vendor'))
+  // var vendorfiles = gulp.src(['app/vendor/**/*'])
+  //     .pipe(gulp.dest('dist/app/vendor'))
 
-    // var exeptionfiles = gulp.src(['app/js/mustache-dojo.js'])
-    //     .pipe(gulp.dest('dist/app/js'))
+  // var exeptionfiles = gulp.src(['app/js/mustache-dojo.js'])
+  //     .pipe(gulp.dest('dist/app/js'))
 
-    // https://github.com/gulpjs/gulp/blob/master/docs/recipes/using-multiple-sources-in-one-task.md
-    //return merge(allfiles, vendorfiles)
-    return allfiles
+  // https://github.com/gulpjs/gulp/blob/master/docs/recipes/using-multiple-sources-in-one-task.md
+  //return merge(allfiles, vendorfiles)
+  return allfiles
+})
+
+gulp.task('pack', () =>
+  gulp.src('dist/app/**/*')
+  .pipe(tar('pack.tar'))
+  .pipe(gzip())
+  .pipe(gulp.dest('build'))
+)
+
+gulp.task('clean', ['clean:dist', 'clean:build'], function() {
+  // place code for your default task here
+})
+
+gulp.task('clean:dist', function() {
+  return gulp.src('dist/**/*', {
+      read: false
+    })
+    .pipe(rm())
+})
+
+gulp.task('clean:build', function() {
+  return gulp.src('build/**/*', {
+      read: false
+    })
+    .pipe(rm())
 })
 
 //https://github.com/andresvia/go-angular-drone/blob/master/.drone.yml
 gulp.task('default', ['webserver'], function() {
-    // place code for your default task here
+  // place code for your default task here
 })
