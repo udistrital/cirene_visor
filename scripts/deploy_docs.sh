fa0da1b8a1091890109afc49e10b44bc20982fad
Jorge Useche <naturalmentejorge@gmail.com>
2017-08-25 15:51:13 -0500

    Arreglar incoherencias de repositorio

--- scripts/deploy_docs.sh
+++ scripts/deploy_docs.sh
@@ -6,8 +6,8 @@
 add_repo() {
   git init
   git checkout -b gh-pages
-  git remote add origin git@github.com:udistrital/cirene_pruebas.git
-  git pull origin master
+  git remote add origin-pages https://${GH_TOKEN}@github.com/udistrital/cirene_pruebas.git > /dev/null 2>&1
+  git pull origin-pages gh-pages
 }
 
 add_files() {
@@ -18,7 +18,6 @@ add_files() {
 
 # /usr/lib64/ruby/gems/2.1.0/gems/travis-1.8.8/bin/travis encrypt SOMEVAR="secretvalue"
 upload_files() {
-  git remote add origin-pages https://${GH_TOKEN}@github.com/udistrital/cirene_pruebas.git > /dev/null 2>&1
   git push --quiet --set-upstream origin-pages gh-pages
 }
 
