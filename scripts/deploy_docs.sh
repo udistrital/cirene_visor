1981bce6426c490d0c03bd87b14e0ab3fc8d2a13
Jorge Useche <naturalmentejorge@gmail.com>
2017-08-25 16:41:26 -0500

    Independiente del nombre del repo.

--- scripts/deploy_docs.sh
+++ scripts/deploy_docs.sh
@@ -1,12 +1,15 @@
 #!/bin/bash -eux
 
-# rationale:
+# rationale: Realiza un commit a la rama gh-pages con la documentación
 # link: https://gist.github.com/willprice/e07efd73fb7f13f917ea
 
+# rationale: Se genera token desde http://github.com/settings/tokens con
+# permisos de accesos a repositorios públicos.
+# /usr/lib64/ruby/gems/2.1.0/gems/travis-1.8.8/bin/travis encrypt GH_TOKEN="secretvalue_github_token"
 add_repo() {
   git init
   git checkout -b gh-pages
-  git remote add origin-pages https://${GH_TOKEN}@github.com/udistrital/cirene_pruebas.git > /dev/null 2>&1
+  git remote add origin-pages https://${GH_TOKEN}@github.com/${TRAVIS_REPO_SLUG}.git > /dev/null 2>&1
   git pull origin-pages gh-pages
 }
 
@@ -16,7 +19,6 @@ add_files() {
   git commit --all --message "Travis build: $TRAVIS_BUILD_NUMBER"
 }
 
-# /usr/lib64/ruby/gems/2.1.0/gems/travis-1.8.8/bin/travis encrypt SOMEVAR="secretvalue"
 upload_files() {
   git push --quiet --set-upstream origin-pages gh-pages
 }
